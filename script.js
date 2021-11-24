3a9bc599768e8b45785303f57588fa3f1bd052f8
Vladyslav Vasylenko <vencovn@gmail.com>
2021-11-24 23:42:50 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-new Splide( '.splide' ).mount();
+new Splide( '#splide-1' ).mount();
+
+new Splide( '#splide-2', {
+    perPage    : 4,
+    breakpoints: {
+        375: {
+            perPage    : 1,
+        },
+    },
+} ).mount();
